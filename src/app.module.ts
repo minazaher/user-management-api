90b19cb38c61f8a01ea11d95c67879e838ab4b25
minazaher <menazaher115@gmail.com>
2025-07-04 19:17:56 +0300

    Add Mongoose integration and user schema for user management

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UsersModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017/CTC-TASK'),
+             UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
