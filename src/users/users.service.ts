0d67c1c1c96d0212a969dac71310bae0aff4dd83
minazaher <menazaher115@gmail.com>
2025-07-04 19:18:17 +0300

    Refactor UsersService to integrate Mongoose for user management and remove in-memory storage

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,42 +1,18 @@
-// users.service.ts
 import { Injectable } from '@nestjs/common';
-
-import { UpdateUserDto } from './dto/update-user.dto';
-
-import { User } from './entity/user.entity';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from './schema/user.schema';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
-  private users: User[] = [];
-  private idCounter = 1;
+  constructor(
+    @InjectModel(User.name) private userModel: Model<UserDocument>,
+  ) {}
 
-  create(createUserDto: CreateUserDto): User {
-    const user: User = {
-      id: this.idCounter++,
-      ...createUserDto,
-    };
-    this.users.push(user);
-    return user;
-  }
-
-  findAll(): User[] {
-    return this.users;``
-  }
-
-  findUserById(id: number): User | undefined {
-    return this.users.find(user => user.id === id);
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto): User | undefined {
-    const user = this.findUserById(id);
-    if (user) {
-        Object.assign(user, updateUserDto);
-    }
-    return user;
-  }
-
-  remove(id: number): void {
-    this.users = this.users.filter(user => user.id !== id);
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const createdUser = new this.userModel(createUserDto);
+    return createdUser.save();
   }
 }
