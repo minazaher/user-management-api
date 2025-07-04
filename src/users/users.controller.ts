2f8eab92b6d90592f381c6f29ebf708e29a293ea
minazaher <menazaher115@gmail.com>
2025-07-05 02:47:17 +0300

    removed  unwanted comment

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -14,7 +14,7 @@ import { UpdateUserDto } from 'src/users/dto/update-user.dto';
 
 import { ApiTags, ApiOperation } from '@nestjs/swagger';
 
-@ApiTags('users') // ✅ Group routes under "users"
+@ApiTags('users') // 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
